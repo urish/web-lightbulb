2b2577baef4ca03b2cbb3eaa623fd4d206873d72
Francois Beaufort <beaufort.francois@gmail.com>
2016-10-05 09:53:23 +0200

    Add Turn On & Off button

--- bulb.js
+++ bulb.js
@@ -1,11 +1,14 @@
 'use strict';
 
 let ledCharacteristic = null;
+let turnedOn = false;
 
 function onConnected() {
     document.querySelector('.connect-button').classList.add('hidden');
     document.querySelector('.color-buttons').classList.remove('hidden');
     document.querySelector('.mic-button').classList.remove('hidden');
+    document.querySelector('.power-button').classList.remove('hidden');
+    turnedOn = false;
 }
 
 function connect() {
@@ -37,6 +40,41 @@ function connect() {
         });
 }
 
+function turnOn() {
+  let data = new Uint8Array([0xcc, 0x23, 0x33]);
+  return ledCharacteristic.writeValue(data)
+      .catch(err => console.log('Error when turning on! ', err))
+      .then(() => {
+          turnedOn = true;
+          toggleButtons();
+      });
+}
+
+function turnOff() {
+  let data = new Uint8Array([0xcc, 0x24, 0x33]);
+  return ledCharacteristic.writeValue(data)
+      .catch(err => console.log('Error when turning off! ', err))
+      .then(() => {
+          turnedOn = false;
+          toggleButtons();
+      });
+}
+
+function turnOnOff() {
+  if (turnedOn) {
+    turnOff();
+  } else {
+    turnOn();
+  }
+}
+
+function toggleButtons() {
+  Array.from(document.querySelectorAll('.color-buttons button')).forEach(function(colorButton) {
+    colorButton.disabled = !turnedOn;
+  });
+  document.querySelector('.mic-button button').disabled = !turnedOn;
+}
+
 function setColor(red, green, blue) {
     let data = new Uint8Array([0x56, red, green, blue, 0x00, 0xf0, 0xaa]);
     return ledCharacteristic.writeValue(data)
@@ -66,7 +104,9 @@ function listen() {
 annyang.addCommands({
     'red': red,
     'green': green,
-    'blue': blue
+    'blue': blue,
+    'turn on': turnOn,
+    'turn off': turnOff
 });
 
 // Install service worker - for offline support
